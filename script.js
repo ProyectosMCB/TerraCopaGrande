05c71fb2af227b8f3171e7e91a310d586f2cfd9f
ProyectosMCB <misaelcrisanto261@gmail.com>
2025-11-06 16:03:04 -0500

    script.js

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
-// Resalta el enlace activo al hacer scroll
-const sections = document.querySelectorAll("section");
-const navLinks = document.querySelectorAll(".nav-link");
 
-window.addEventListener("scroll", () => {
-  let current = "";
-
-  sections.forEach((section) => {
-    const sectionTop = section.offsetTop - 100;
-    if (scrollY >= sectionTop) {
-      current = section.getAttribute("id");
-    }
-  });
-
-  navLinks.forEach((link) => {
-    link.classList.remove("active");
-    if (link.getAttribute("href").includes(current)) {
-      link.classList.add("active");
-    }
-  });
-});
 
 // Scroll suave en la navegación
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
